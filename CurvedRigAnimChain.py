8323de2074586f2dcfa7d7984b40d88beaa2ebfb
Wei Zeng <wzeng11@my.bcit.ca>
2024-02-20 23:50:31 -0800

    edit animating next button

--- CurvedRigAnimChain.py
+++ CurvedRigAnimChain.py
@@ -6,11 +6,18 @@ global isFirstTime
 global ChainCount
 global selected_curve_name
 global curve_type
+global first_time_make_chain
+global proxy_geo
+global mesh_selected
+global user_selected_mesh
 
 isFirstTime = True
 ChainCount = 22
 selected_curve_name = ""
 curve_type = 3
+first_time_make_chain = True
+proxy_geo = None
+user_selected_mesh = None
 
 
 def find_object_in_scene():
@@ -87,26 +94,23 @@ def make_window():
         cmds.setParent(master_layout)
 
         # *****************************************************************************************************************
-        make_window.ui_controller_frame = cmds.frameLayout(l="Make Controllers", w=700, cll=True, cl=True)
+        make_window.ui_controller_frame = cmds.frameLayout(l="Controller & Chains", w=700, cll=True, cl=True)
         make_window.ui_controller_layout = cmds.columnLayout()
         cmds.text("now if you are done with the curve, use this button to create controllers")
         make_window.RedoCTRL = cmds.intSliderGrp(min=3, max=100, v=3, cc="CTRLRedo()", f=True, en=False)
-        cmds.rowLayout(numberOfColumns=2)
+        cmds.rowLayout(numberOfColumns=3)
         make_window.btn_cmaker = cmds.button(l="CTRL maker", c="CMaker()", en=False)
         make_window.btn_to_final_step = cmds.button(l="Next", c="make_chain_section()", en=False)
+        make_window.btn_update = cmds.button(l="Update Chain", c="update_chain()", en=False)
         cmds.setParent(make_window.ui_controller_layout)
         cmds.separator(w=700)
-        cmds.setParent(master_layout)
-
-        # *****************************************************************************************************************
-        make_window.ui_chain_frame = cmds.frameLayout(l="Make Chains", w=700, cll=True, cl=True)
-        make_window.ui_chain_layout = cmds.columnLayout()
         cmds.text("now if you are done, select your desired object and click next")
-        cmds.rowLayout(numberOfColumns=3)
-        make_window.btn_make_chain_from_selected_obj = cmds.button(l="Make Chain", c="FinalStep()", en=False)
+        cmds.setParent(make_window.ui_controller_layout)
+        cmds.rowLayout(numberOfColumns=2)
+        # make_window.btn_make_chain_from_selected_obj = cmds.button(l="Make Chain", c="FinalStep()", en=False)
         make_window.btn_make_chain = cmds.button(l="MakeChain Only", c="MakeChain()", en=False)
         make_window.cb_make_proxy = cmds.checkBox(l="Use Proxy Geo", cc="make_proxy_geo()", en=False)
-        cmds.setParent(make_window.ui_chain_layout)
+        cmds.setParent(make_window.ui_controller_layout)
         cmds.setParent(master_layout)
 
         # *****************************************************************************************************************
@@ -183,13 +187,14 @@ def expand_controller_section():
 
 def make_chain_section():
     # Expand toggles
-    cmds.frameLayout(make_window.ui_controller_frame, e=True, cl=True)
-    cmds.frameLayout(make_window.ui_chain_frame, e=True, cl=False)
+    # cmds.frameLayout(make_window.ui_controller_frame, e=True, cl=True)
     cmds.intSliderGrp(make_window.RedoCTRL, e=True, en=False)
     cmds.button(make_window.btn_to_final_step, e=True, en=False)
-    cmds.button(make_window.btn_make_chain_from_selected_obj, e=True, en=True)
+    # cmds.button(make_window.btn_make_chain_from_selected_obj, e=True, en=True)
     cmds.button(make_window.btn_make_chain, e=True, en=True)
     cmds.checkBox(make_window.cb_make_proxy, e=True, en=True)
+
+    cmds.confirmDialog(t="Error", m="You should select one mesh!")
 
 
 def SmoothChange():
@@ -211,6 +216,8 @@ def CMaker():
 
     cmds.floatSliderGrp(make_window.SmoothNess, e=True, en=False)
     cmds.intSliderGrp(make_window.RedoCTRL, e=True, en=False)
+    cmds.button(make_window.btn_to_final_step, e=True, en=True)
+
 
     selectedCRV = cmds.ls(sl=True)
     EPlist = cmds.ls(selectedCRV[0] + ".ep[*]", fl=True)
@@ -225,51 +232,92 @@ def CMaker():
     cmds.select(locList)
     cmds.group(n="AllLocators")
     cmds.select(selectedCRV)
+    cmds.select(cl=True)
     cmds.intSliderGrp(make_window.RedoCTRL, e=True, en=True)
     cmds.button(make_window.btn_cmaker, e=True, en=False)
 
 
 def CTRLRedo():
-    cmds.button(make_window.btn_to_final_step, e=True, en=True)
-    cmds.delete("AllLocators")
+    global selected_curve_name
+
+    try:
+        cmds.delete("AllLocators")
+    except:
+        pass
     newRespan = cmds.intSliderGrp(make_window.RedoCTRL, q=True, v=True)
+    cmds.select(selected_curve_name)
     selectedcurve = cmds.ls(sl=True)
+
     cmds.rebuildCurve(selectedcurve, rpo=True, kep=True, rt=0, s=newRespan)
     cmds.select(selectedcurve)
     CMaker()
 
+def update_chain():
+    global proxy_geo, mesh_selected, user_selected_mesh
 
-def FinalStep():
+    cmds.button(make_window.btn_to_final_step, e=True, en=False)
+    try:
+        cmds.delete("AllLinks")
+        cmds.select(user_selected_mesh)
+    except:
+        pass
 
-    global ChainCount
-    global selected_curve_name
+    cmds.delete(mp=True)
 
-    cmds.button(make_window.btn_make_chain, e=True, en=True)
-    cmds.button(make_window.btn_cmaker, e=True, en=False)
-    cmds.intSliderGrp(make_window.RedoCTRL, e=True, en=False)
+    if user_selected_mesh == proxy_geo:
+        cmds.select(proxy_geo)
+        cmds.setAttr(proxy_geo[0]+".translateX", 0.0)
+        cmds.setAttr(proxy_geo[0] + ".translateY", 0.0)
+        cmds.setAttr(proxy_geo[0] + ".translateZ", 0.0)
+        cmds.setAttr(proxy_geo[0] + ".rotateX", 0.0)
+        cmds.setAttr(proxy_geo[0] + ".rotateY", 0.0)
+        cmds.setAttr(proxy_geo[0] + ".rotateZ", 0.0)
 
-    startTime = 1
-    AnimCTList = cmds.ls("EPCTRL*", tr=True)
-    endTime = len(AnimCTList)
+    CTRLRedo()
 
-    chainLinks = []
+    cmds.select(user_selected_mesh)
 
-    selectedOBJ = cmds.ls(sl=True)
-    for CurKey in range(startTime, endTime + 1):
-        myTime = cmds.currentTime(query=True)
-        cmds.select(selectedOBJ, r=True)
-        cmds.FreezeTransformations()
-        cmds.duplicate()
-        cmds.select(AnimCTList[CurKey - 1], add=True)
-        cmds.matchTransform()
-        cmds.parentConstraint(w=1.0)
-        cmds.currentTime(myTime + 1, e=True)
-        cmds.playbackOptions(by=1)
-        chainLinks.append(cmds.rename("CLink1"))
-    
-    cmds.select(chainLinks)
-    cmds.group(n="AllLinks")
-    cmds.currentTime(startTime)
+    MakeChain()
+
+def scale_chain(scale_value):
+    Clink_lists= cmds.ls("CLink*",  fl=True, type="transform")
+    for i in Clink_lists:
+        cmds.setAttr("{}.scaleX".format(i), scale_value)
+        cmds.setAttr("{}.scaleY".format(i), scale_value)
+        cmds.setAttr("{}.scaleZ".format(i), scale_value)
+
+
+# def FinalStep():
+#
+#     global ChainCount
+#     global selected_curve_name
+#
+#     cmds.button(make_window.btn_make_chain, e=True, en=True)
+#     cmds.button(make_window.btn_cmaker, e=True, en=False)
+#     cmds.intSliderGrp(make_window.RedoCTRL, e=True, en=False)
+#
+#     startTime = 1
+#     AnimCTList = cmds.ls("EPCTRL*", tr=True)
+#     endTime = len(AnimCTList)
+#
+#     chainLinks = []
+#
+#     selectedOBJ = cmds.ls(sl=True)
+#     for CurKey in range(startTime, endTime + 1):
+#         myTime = cmds.currentTime(query=True)
+#         cmds.select(selectedOBJ, r=True)
+#         cmds.FreezeTransformations()
+#         cmds.duplicate()
+#         cmds.select(AnimCTList[CurKey - 1], add=True)
+#         cmds.matchTransform()
+#         cmds.parentConstraint(w=1.0)
+#         cmds.currentTime(myTime + 1, e=True)
+#         cmds.playbackOptions(by=1)
+#         chainLinks.append(cmds.rename("CLink1"))
+#
+#     cmds.select(chainLinks)
+#     cmds.group(n="AllLinks")
+#     cmds.currentTime(startTime)
     
 
 # def MakeChain():
@@ -311,48 +359,69 @@ def FinalStep():
 #         cmds.setAttr(chainLinks[i] + ".rx", 90)
 
 def MakeChain():
-    global ChainCount
-    global selected_curve_name
+    global ChainCount, selected_curve_name, first_time_make_chain, mesh_selected, user_selected_mesh
 
-    selected_curve_name = cmds.textScrollList(make_window.curve_scroll_list, q=True, si=True)
-    selectedOBJ = cmds.ls(sl=True)
-    cmds.select(selectedOBJ, r=True)
-    cmds.select(selected_curve_name, add=True)
-    cmds.pathAnimation(fm=True, f=True, fa="x", ua="y", wut="vector", wu=(0, 1, 0), inverseFront=False, iu=False,
-                       b=False, stu=1, etu=ChainCount)
-    cmds.select(selectedOBJ, r=True)
+    user_selected_mesh = cmds.ls(sl=True)
+    cmds.button(make_window.btn_to_final_step, e=True, en=False)
+    # mesh_selected = len(cmds.ls(sl=True))
+    # obj = cmds.ls(sl=True)[0]
+    # obj_type = cmds.ls(obj[0:-1] + "Shape" + obj[-1], type='geometryShape', showType=True)
 
-    AnimCTList = cmds.ls("EPCTRL*", tr=True)
-    cmds.selectKey('motionPath1_uValue', time=(1, ChainCount))
-    cmds.keyTangent(itt="linear", ott="linear")
-    chainLinks = []
+    # if mesh_selected == 1 and obj_type[1] == "mesh":
+    if user_selected_mesh:
 
-    for curkey in range(1, ChainCount + 1):
-        cmds.currentTime(curkey)
+        cmds.button(make_window.btn_update, e=True, en=True)
+
+        selected_curve_name = cmds.textScrollList(make_window.curve_scroll_list, q=True, si=True)
+        selectedOBJ = cmds.ls(sl=True)
         cmds.select(selectedOBJ, r=True)
-        cmds.FreezeTransformations()
-        cmds.duplicate()
-        cmds.select(AnimCTList[curkey - 1], add=True)
-        cmds.parentConstraint(w=1.0)
-        chainLinks.append(cmds.rename("CLink1"))
-    cmds.select(chainLinks)
+        cmds.select(selected_curve_name, add=True)
+        cmds.pathAnimation(fm=True, f=True, fa="x", ua="y", wut="vector", wu=(0, 1, 0), inverseFront=False, iu=False,
+                           b=False, stu=1, etu=ChainCount)
+        cmds.select(selectedOBJ, r=True)
 
-    cmds.group(n="AllLinks")
-    linksCount = len(chainLinks)
-    for i in range(1, linksCount, 2):
-        cmds.currentTime(i)
-        cmds.select(chainLinks[i])
-        cmds.setAttr(chainLinks[i] + ".rx", 90)
-    cmds.DeleteMotionPaths()
+        AnimCTList = cmds.ls("EPCTRL*", tr=True)
+        cmds.selectKey('motionPath1_uValue', time=(1, ChainCount))
+        cmds.keyTangent(itt="linear", ott="linear")
+        chainLinks = []
+
+        for curkey in range(1, ChainCount + 1):
+            cmds.currentTime(curkey)
+            cmds.select(selectedOBJ, r=True)
+            cmds.FreezeTransformations()
+            cmds.duplicate()
+            cmds.select(AnimCTList[curkey - 1], add=True)
+            cmds.parentConstraint(w=1.0)
+            chainLinks.append(cmds.rename("CLink1"))
+
+        cmds.select(chainLinks)
+        cmds.group(n="AllLinks")
+        linksCount = len(chainLinks)
+
+        for i in range(1, linksCount, 2):
+            cmds.currentTime(i)
+            cmds.select(chainLinks[i])
+            cmds.setAttr(chainLinks[i] + ".rx", 90)
+        cmds.DeleteMotionPaths()
+
+        first_time_make_chain = False
+        cmds.button(make_window.btn_make_chain, e=True, en=False)
+        cmds.intSliderGrp(make_window.RedoCTRL, e=True, en=True)
+        cmds.checkBox(make_window.cb_make_proxy, e=True, en=False)
+
+        scale_chain(10)
 
 
 def make_proxy_geo():
+    global proxy_geo
+
     proxy_geo = cmds.polyTorus(n="ProxyGeo", sa=10, sh=10, sr=0.3)
     edges_to_scale = []
     edges_template = ["pTorus1.e[109]", "pTorus1.e[119]", "pTorus1.e[129]", "pTorus1.e[139]", "pTorus1.e[149]",
                       "pTorus1.e[159]", "pTorus1.e[169]", "pTorus1.e[179]", "pTorus1.e[189]", "pTorus1.e[199]",
                       "pTorus1.e[104]", "pTorus1.e[114]", "pTorus1.e[124]", "pTorus1.e[134]", "pTorus1.e[144]",
                       "pTorus1.e[154]", "pTorus1.e[164]", "pTorus1.e[174]", "pTorus1.e[184]", "pTorus1.e[194]"]
+
     for edge in edges_template:
         suffix_name = edge.split(".")[-1]
         new_edge_name = "ProxyGeo." + str(suffix_name)
@@ -362,6 +431,8 @@ def make_proxy_geo():
     cmds.select(edges_to_scale)
     cmds.scale(0.828, 1, 1, )
     cmds.select(proxy_geo)
+
+    cmds.checkBox(make_window.cb_make_proxy, e=True, en=False)
 
 
 def refresh_list():
